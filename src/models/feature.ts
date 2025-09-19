409a04c3c8858ac55a9e4210a11eb45e95cc9bab
santoshvarmaaddala <santoshvarma2166@gmail.com>
2025-09-19 12:28:18 +0530

    launcher config and feature model added

---
+++ src/models/feature.ts
@@ -0,0 +1,19 @@
+export interface Note {
+    id: string;
+    content: string;
+    createdAt: string;
+}
+
+export type FeatureType = "feature" | "bug" | "idea" | "task";
+export type FeatureStatus = "todo" | "in-progress" | "completed";
+
+export interface Feature {
+    id: string;
+    title: string;
+    type: FeatureType;
+    status: FeatureStatus;
+    priority?: "low" | "medium" | "high";
+    createdAt: string;
+    updatedAt: string;
+    notes: Note[]
+}
